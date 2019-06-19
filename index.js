7b84108f1f6844890321bbfa49d74775fb855b4d
Levi Bernard <levichristianbernard@gmail.com>
2019-06-19 10:29:26 -0500

    decimal and operator key press

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
 
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
     const key = e.target;
     const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    Array.from(key.parentNode.children).forEach(k =>
+      k.classList.remove('is-depressed')
+    );
     if (!action) {
-      console.log('number key!');
+      if (displayedNum === '0') {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
     }
     if (
       action === 'add' ||
@@ -14,10 +24,10 @@ keys.addEventListener('click', e => {
       action === 'multiply' ||
       action === 'divide'
     ) {
-      console.log('operator key!');
+      key.classList.add('is-depressed');
     }
     if (action === 'decimal') {
-      console.log('decimal key!');
+      display.textContent = displayedNum + '.';
     }
     if (action === 'clear') {
       console.log('clear key!');
@@ -25,6 +35,5 @@ keys.addEventListener('click', e => {
     if (action === 'calculate') {
       console.log('equal key!');
     }
-    //do something
   }
 });
