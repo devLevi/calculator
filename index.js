37e293cc96e7b0f8240897943b202b5ab6f0a9eb
Levi Bernard <levichristianbernard@gmail.com>
2019-06-19 21:42:46 -0500

    added calculate function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+const calculate = (n1, operator, n2) => {
+  let result = '';
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2);
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2);
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2);
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2);
+  }
+  return result;
+};
+
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
 const display = document.querySelector('.calculator__display');
@@ -8,11 +22,14 @@ keys.addEventListener('click', e => {
     const action = key.dataset.action;
     const keyContent = key.textContent;
     const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
     Array.from(key.parentNode.children).forEach(k =>
       k.classList.remove('is-depressed')
     );
+
     if (!action) {
-      if (displayedNum === '0') {
+      if (displayedNum === '0' || previousKeyType === 'operator') {
         display.textContent = keyContent;
       } else {
         display.textContent = displayedNum + keyContent;
@@ -25,6 +42,10 @@ keys.addEventListener('click', e => {
       action === 'divide'
     ) {
       key.classList.add('is-depressed');
+      //add custom attribute
+      calculator.dataset.previousKeyType = 'operator';
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
     }
     if (action === 'decimal') {
       display.textContent = displayedNum + '.';
@@ -33,7 +54,10 @@ keys.addEventListener('click', e => {
       console.log('clear key!');
     }
     if (action === 'calculate') {
-      console.log('equal key!');
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+      display.textContent = calculate(firstValue, operator, secondValue);
     }
   }
 });
